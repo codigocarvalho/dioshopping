3c6584eda6f9915fea61934862a680af61145949
Henrique Carvalho <orickcarvalho@gmail.com>
2022-02-12 19:29:41 -0300

    Atualizado as dependências

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import { ListItem, ListItemText } from '@material-ui/core/';
+import React from "react";
+import { ListItem, ListItemText } from "@material-ui/core/";
 
-const Item = ({ name, details }) => {
-    return(
-        <ListItem>
-            <ListItemText
-                primary={name}
-                secondary={details}
-            />
-        </ListItem>
-    )
-}
+const Item = ({ name, details }) => (
+  <ListItem>
+    <ListItemText primary={name} secondary={details} />
+  </ListItem>
+);
 
 export default Item;
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,13 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './Pages/home';
-import Contatos from './Pages/contato';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/home";
+import Contatos from "./Pages/contato";
 
-const MainRoutes = () => {
-    return(
-        <Routes>
-            <Route
-                path="/"
-                element={<HomePage />}
-            />
-            <Route
-                path="/contato"
-                element={<Contatos />}
-            />
-        </Routes>
-    )
-}
+const MainRoutes = () => (
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/contato" element={<Contatos />} />
+  </Routes>
+);
 
 export default MainRoutes;
